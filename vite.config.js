000a6ce9652f4ce2ca717b386858759272bbd54b
pandu926 <63030609+pandu926@users.noreply.github.com>
2024-05-31 16:57:08 +0700

    ya

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,35 @@
-// import { readFileSync } from 'node:fs';
-import { dirname, resolve } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import react from '@vitejs/plugin-react-swc';
-import { defineConfig } from 'vite';
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import react from "@vitejs/plugin-react-swc";
+import { defineConfig } from "vite";
+import fs from "fs";
 
-// https://vitejs.dev/config/
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+// Cek apakah sertifikat dan kunci HTTPS ada
+const certPath = resolve(__dirname, "./cert.pem");
+const keyPath = resolve(__dirname, "./key.pem");
+
+const httpsConfig =
+  fs.existsSync(certPath) && fs.existsSync(keyPath)
+    ? {
+        cert: fs.readFileSync(certPath),
+        key: fs.readFileSync(keyPath),
+      }
+    : undefined;
+
 export default defineConfig({
-  base: '/reactjs-js-template/',
+  base: "/reactjs-js-template/",
   plugins: [react()],
-  // Uncomment the next lines in case, you would like to run Vite dev server using HTTPS and in case,
-  // you have key and certificate. You retrieve your certificate and key using mkcert.
-  // Learn more:
-  // https://docs.telegram-mini-apps.com/platform/getting-app-link#mkcert
-  //
-  // server: {
-  //   port: 443,
-  //   https: {
-  //     cert: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal.pem')),
-  //     key: readFileSync(resolve(dirname(fileURLToPath(import.meta.url)), './tma.internal-key.pem')),
-  //   },
-  //   host: 'tma.internal',
-  // },
+  server: {
+    port: 443,
+    https: httpsConfig,
+    host: "192.168.47.232",
+  },
   resolve: {
     alias: {
-      '@': resolve(dirname(fileURLToPath(import.meta.url)), './src'),
-    }
+      "@": resolve(__dirname, "./src"),
+    },
   },
-  publicDir: './public',
+  publicDir: "./public",
 });
-
